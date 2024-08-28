f05c2b0b9511fe438ccc1f360e09bf4dc4947ecc
deathwhispers <guangjian.yang@qq.com>
2024-08-28 15:16:55 +0800

    自定义参数校验

---
+++ src/main/java/com/guangjian/gtool/validator/NodeTypeValidator.java
@@ -0,0 +1,57 @@
+package com.guangjian.gtool.validator;
+
+import cn.hutool.core.util.StrUtil;
+import com.guangjian.gtool.validator.constraints.NodeTypeConstraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+/**
+ * 组织机构节点类型校验器
+ * {@link NodeTypeConstraint}
+ *
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2022/12/2 9:53
+ */
+public class NodeTypeValidator implements ConstraintValidator<NodeTypeConstraint, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return StrUtil.isBlank(value) || NodeTypeEnum.include(value);
+    }
+}
+@Getter
+@RequiredArgsConstructor
+enum NodeTypeEnum {
+
+    // 节点类型 枚举类
+    GROUP("0", "group", "集团"),
+    ORG("1", "org", "机构组织(运营公司)"),
+    WAY("2", "way", "路段"),
+    FAC("3", "fac", "设施(地理位置)");
+
+    private final String code;
+    private final String name;
+    private final String desc;
+
+    public static String getName(String code) {
+        for (NodeTypeEnum value : values()) {
+            if (value.getCode().equals(code)) {
+                return value.getName();
+            }
+        }
+        return null;
+    }
+
+    public static boolean include(String code) {
+        return Arrays.stream(values()).anyMatch(e -> e.getCode().equals(code));
+    }
+
+    public static boolean excludeByName(String name) {
+        return Arrays.stream(values()).noneMatch(e -> e.getName().equals(name));
+    }
+}
---
+++ src/main/java/com/guangjian/gtool/validator/constraints/TemporalUnitConstraint.java
@@ -0,0 +1,33 @@
+package com.guangjian.gtool.validator.constraints;
+
+
+import com.guangjian.gtool.validator.TemporalUnitValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * 时间单位约束
+ *
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2022/12/2 9:50
+ */
+@Documented
+@Constraint(validatedBy = {TemporalUnitValidator.class})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+public @interface TemporalUnitConstraint {
+
+    String message() default "{com.rhy.aibox.common.validator.constraints.TemporalUnitConstraint.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
