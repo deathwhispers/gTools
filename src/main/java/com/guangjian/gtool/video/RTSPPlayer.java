732579ac955d6d3512ccbc6122eb49b67ddde668
杨光剑 <yangguangjian@rhytech.com.cn>
2024-08-28 15:18:39 +0800

    pom

---
+++ src/main/java/com/guangjian/gtool/video/RTSPPlayer.java
@@ -0,0 +1,15 @@
+package com.guangjian.gtool.video;
+
+
+/**
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2024/1/9 10:30
+ */
+
+public class RTSPPlayer {
+
+    public static void main(String[] args) {
+
+    }
+}
